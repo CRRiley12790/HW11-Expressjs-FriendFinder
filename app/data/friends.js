ef3861aa2a582063f02ebeb52dd43ad857ec3135
CRiley <CRRiley12790@gmail.com>
2018-07-26 18:56:59 -0400

    HW11

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,41 @@
-var friends = {}
+
+var friendsHIMYM = [
+    {
+      name: "Ted Mosby",
+      photo: "https://cdn.lifehack.org/wp-content/uploads/2014/09/ted-mosby.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Barney Stinson",
+      photo:"https://eteknix-eteknixltd.netdna-ssl.com/wp-content/uploads/2015/03/Barney-Stinson.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Marshall Ericksen",
+      photo:"http://www.pinkmyrideph.com/wp-content/uploads/2014/04/Marshall-Eriksen-Judge.png",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Lilly Aldrin",
+      photo:"https://chekhovsgunman.files.wordpress.com/2013/11/lily-aldrin-how-i-met-your-mother-wedding-alyson-hannigan.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Robin Scherbatski",
+      photo:"http://himym.aarongoldsman.com/wp-content/uploads/2014/04/robin-1038x576.png",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Tracy McConnell",
+      photo:"https://upload.wikimedia.org/wikipedia/it/thumb/4/4c/How_I_Met_Your_Mother_-_The_Mother.png/260px-How_I_Met_Your_Mother_-_The_Mother.png",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Ranjit",
+      photo:"http://himym.aarongoldsman.com/wp-content/uploads/2014/05/tumblr_ms807dC4g31s9d3o5o1_500.gif",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+// Export to API Route
+module.exports = friendsHIMYM;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,72 @@
+console.log('API Route Connected Successfully');
+
+
+// Link to Friends Data
+var friendsHIMYM = require('../data/friends.js');
+
+
+//Includes Two Routes
+function apiRoutes(app) {
+
+    //Get Route with the url /api/friends. Used to display a JSON of all possible friends.
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsHIMYM);
+    });
+
+    //POST routes /api/friends. This will be used to handle incoming survey results.
+    app.post('/api/friends', function (req, res) {
+
+        //Parse new friend input to get integers
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+        var scoresArray = [];
+        for (var i = 0; i < req.body.scores.length; i++) {
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        newFriend.scores = scoresArray;
+
+
+        //Cross check the new friend entry with the existing ones
+        var scoreComparisionArray = [];
+        for (var i = 0; i < friendsData.length; i++) {
+
+            //Check each friend's scores/difference in points
+            var currentComparison = 0;
+            for (var j = 0; j < newFriend.scores.length; j++) {
+                currentComparison += Math.abs(newFriend.scores[j] - friendsData[i].scores[j]);
+            }
+
+            //Push each comparison between friends
+            scoreComparisionArray.push(currentComparison);
+        }
+
+        //Determine the best match using the postion of best match in the friendsData array
+        var bestMatchPosition = 0;
+        for (var i = 1; i < scoreComparisionArray.length; i++) {
+
+            //Lower number = better match
+            if (scoreComparisionArray[i] <= scoreComparisionArray[bestMatchPosition]) {
+                bestMatchPosition = i;
+            }
+
+        }
+
+        //If the 2 friends have the same comparison, then the NEWEST entry in the friendsData array is chosen
+        var bestFriendMatch = friendsData[bestMatchPosition];
+
+
+
+        //Reply with a JSON object of the best match
+        res.json(bestFriendMatch);
+
+
+
+        //Push the new friend to the friends data array for storage
+        friendsData.push(newFriend);
+    });
+}
+//Export for use in server.js file
+module.exports = apiRoutes;
